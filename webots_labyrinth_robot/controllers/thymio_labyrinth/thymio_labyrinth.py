ae0c915f28cd5099d1ede3c9559efb1a1d4401e0
Siruhel <Helka.siru@gmail.com>
2021-11-21 12:40:16 +0200

    code cleanup

--- webots_labyrinth_robot/controllers/thymio_labyrinth/thymio_labyrinth.py
+++ webots_labyrinth_robot/controllers/thymio_labyrinth/thymio_labyrinth.py
@@ -1,7 +1,5 @@
 """thymio_labyrinth controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 import time
 
@@ -22,55 +20,32 @@ leftWheel = robot.getDevice("left wheel motor")
 rightWheel = robot.getDevice("right wheel motor")
 
 frontRightS= robot.getDevice("ps0")
-centralRightS= robot.getDevice("ps1")
-rightS= robot.getDevice("ps2")
-rightBackS= robot.getDevice("ps3")
-leftBackS= robot.getDevice("ps4")
 leftS= robot.getDevice("ps5")
 centralLeftS= robot.getDevice("ps6")
 frontLeftS= robot.getDevice("ps7")
 
 # Enable distance sensors.
 frontRightS.enable(timestep)
-centralRightS.enable(timestep)
-rightS.enable(timestep)
-rightBackS.enable(timestep)
-leftBackS.enable(timestep)
 leftS.enable(timestep)
 centralLeftS.enable(timestep)
 frontLeftS.enable(timestep)
-
 
 # Disable motor PID control mode.
 leftWheel.setPosition(float('inf'))
 rightWheel.setPosition(float('inf'))
 
-# Set ideal motor velocity.
-initialVelocity =  MAX_SPEED
-
-
-
+# convert the value from the distance sensors into values that are easier to compare. 
+# values under 100 are ignored as white noise
 def getDistance(sensor):
-    """
-    Return the distance of an obstacle for a sensor.
-
-    The value returned by the getValue() method of the distance sensors
-    corresponds to a physical value (here we have a sonar, so it is the
-    strength of the sonar ray). This function makes a conversion to a
-    distance value in meters.
-    """
-    #print(sensor.getValue())
     if (sensor.getValue() > 100):
         return round(sensor.getValue()/50, 2)
     else:
         return 0
 
-
-
 leftWheel.setVelocity(MAX_SPEED)
 rightWheel.setVelocity(MAX_SPEED)
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+
+# robot will first check if there is a wall on its left and find one if not
 if (getDistance(leftS) < 8.8):
     if(getDistance(frontLeftS) < 1): 
         leftWheel.setVelocity(MAX_SPEED)
@@ -84,47 +59,33 @@ while robot.step(timestep) != -1:
         if(getDistance(frontRightS) > 1):
             break;
 
-
+# Main loop:
+# robot will then proceed to follow the wall on its left side 
 while robot.step(timestep) != -1:
+    
+    # if robot finds a wall straight in front of it, it will turn right
     if(getDistance(frontRightS) > 1):
         rightWheel.setVelocity(0)
-        leftWheel.setVelocity(MAX_SPEED)
-        print("there wall ahead")
-        
-        
+        leftWheel.setVelocity(MAX_SPEED)        
+    
     elif(getDistance(leftS) < 8.8):
+        # if robot senses it is going too much towards the wall it will attempt to turn parallel to the wall
         if(getDistance(centralLeftS) > 4.5):
             if(getDistance(frontLeftS) > 2):
                 rightWheel.setVelocity(MAX_SPEED * 0.2)
                 leftWheel.setVelocity(MAX_SPEED)
-                print("really going at the wall")
             else:
-                print("kinda going at the wall")
                 rightWheel.setVelocity(MAX_SPEED * 0.4)
                 leftWheel.setVelocity(MAX_SPEED)
+        # if there is no wall on the left side, the robot will turn left 
         else:
             leftWheel.setVelocity(MAX_SPEED*0.2)
             rightWheel.setVelocity(MAX_SPEED)
             
-            print("no wall on left must do turn")
-            
-    
-    
     else:
         if(getDistance(centralLeftS) > 4.5):
             rightWheel.setVelocity(MAX_SPEED * 0.6)
             leftWheel.setVelocity(MAX_SPEED)
-            print("too close to wall")
         else:
-            print("just go straight all good")
             leftWheel.setVelocity(MAX_SPEED)
             rightWheel.setVelocity(MAX_SPEED)
-
-
-# Enter here exit cleanup code.
-
-# leftsensor value should be around 10 
-# centralleftsensor value should be around 4 
-
-# need to fix: must check if value from centralleftsensor is too big outside the lop of checking 
-# for leftsensor value: robot goes to the wall because it doesnät care of the value if left is ok 
